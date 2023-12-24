b1cbb2cd455e706dec99c11f0b346ee094df0f1b
Girinath Selvendran <89913730+girinathselvendran@users.noreply.github.com>
2023-12-24 15:29:03 +0530

    latex examples added

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,13 @@
+// var Latex = require("react-latex");
+// import Latex from "react-latex";
+import { BlockMath, InlineMath } from "react-katex";
+
 export default function Profile() {
-  return <div>Profile</div>;
+  return (
+    <div>
+      <h2>LaTeX Example:</h2>
+      <BlockMath math="\frac{-b \pm \sqrt{b^2-4 a c}}{2 a}" />
+      <img src="https://cdn.mathpix.com/snip/images/pFliEOBvLufS20P9VjsDefFfE5j-jULWLZqiamXaarc.original.fullsize.png" />
+    </div>
+  );
 }
